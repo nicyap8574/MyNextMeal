03660c3b928fde376d29eb92874d31cfe8cbb1c3
nicyap8574 <nic8574@gmail.com>
2026-04-03 17:34:30 +0800

    Added sizes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mynextmeal/utils/constants/colors.dart';
 import 'package:mynextmeal/utils/theme/theme.dart';
 
 import 'app.dart';
 
 void main() {
-
 
   runApp(const App());
 

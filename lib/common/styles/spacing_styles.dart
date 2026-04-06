72c90b26a4b2bb09c347dbe56f7249b1678b25bd
nicyap8574 <nic8574@gmail.com>
2026-04-06 14:48:26 +0800

    Latest update

---
+++ lib/common/styles/spacing_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../utils/constants/sizes.dart';
+
+class AppSpacingStyle{
+  static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
+    top: AppSizes.appBarHeight,
+    left: AppSizes.defaultSpace,
+    bottom: AppSizes.defaultSpace,
+    right: AppSizes.defaultSpace,
+  );
+}

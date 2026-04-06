e806814aecd8c0bd947725936650324ab20fd9b9
nicyap8574 <nic8574@gmail.com>
2026-04-06 15:28:43 +0800

    Launcher icon

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -12,6 +12,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.celadon700,
       body: SingleChildScrollView(
         child: Padding(
           padding: AppSpacingStyle.paddingWithAppBarHeight,

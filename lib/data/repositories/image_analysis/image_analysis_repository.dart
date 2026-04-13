e2c9347a85c0961c4906ffa74135254ab5cc1afc
nicyap8574 <nic8574@gmail.com>
2026-04-13 13:46:24 +0800

    Added chips for JSON output

--- lib/data/repositories/image_analysis/image_analysis_repository.dart
+++ lib/data/repositories/image_analysis/image_analysis_repository.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:firebase_ai/firebase_ai.dart';
--- lib/screens/image_analysis.dart
+++ lib/screens/image_analysis.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
@@ -63,7 +65,83 @@ class ImageAnalysis extends StatelessWidget {
                 if(repo.isLoading.value == true){
                   return Text("Response is loading...");
                 }else{
-                  return Text(repo.response.value);
+
+
+                  final data = jsonDecode(repo.response.value);
+                  final nutrients = data['nutrients'] as List<dynamic>;
+                  final meal = nutrients[0] as Map<String, dynamic>;
+                  final mealName = meal['meal_name'];
+                  final ingredients = meal['detected_ingredients'] as List<dynamic>;
+                  final carbsMacro = meal['carbs_macro'];
+                  final proteinMacro = meal['protein_macro'];
+                  final fatsMacro = meal['fats_macro'];
+                  final mealHealthiness = meal['meal_healthiness'];
+                  final confidenceLevel = meal['confidence_level'];
+                  final briefSummary = meal['brief_summary'];
+
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Dish Name"),
+                      Chip(
+                        label: Text(mealName),
+                      ),
+
+                      const SizedBox(height: AppSizes.spaceBtwItems),
+
+                      Text("Ingredients"),
+                      Wrap(
+                        spacing: 8,
+                        children: ingredients.map((individual_ingredient){
+                          return Chip(
+                            label: Text(individual_ingredient),
+                          );
+                        }).toList(), //converts Iterable to List<Widget> to be accepted by children
+                      ),
+
+                      const SizedBox(height: AppSizes.spaceBtwItems),
+
+                      Text("Carbs Macro"),
+                      Chip(
+                        label: Text(carbsMacro),
+                      ),
+
+                      const SizedBox(height: AppSizes.spaceBtwItems),
+
+                      Text("Protein Macro"),
+                      Chip(
+                        label: Text(proteinMacro),
+                      ),
+
+                      const SizedBox(height: AppSizes.spaceBtwItems),
+
+                      Text("Fats Macro"),
+                      Chip(
+                        label: Text(fatsMacro),
+                      ),
+
+                      const SizedBox(height: AppSizes.spaceBtwItems),
+
+                      Text("Meal Healthiness"),
+                      Chip(
+                        label: Text(mealHealthiness),
+                      ),
+
+                      const SizedBox(height: AppSizes.spaceBtwItems),
+
+                      Text("Confidence Level"),
+                      Chip(
+                        label: Text(confidenceLevel),
+                      ),
+
+                      const SizedBox(height: AppSizes.spaceBtwItems),
+
+                      Text("Brief Summary"),
+                      Text(briefSummary),
+                    ]
+                  );
+
+                  //return Text(repo.response.value);
                 }
               }),
             ]

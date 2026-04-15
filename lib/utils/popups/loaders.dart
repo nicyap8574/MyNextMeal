1f44b1d77993b7f069356e1cbf2ba22160fabe1d
nicyap8574 <nic8574@gmail.com>
2026-04-15 15:04:35 +0800

    Removed old snackbars

--- lib/utils/popups/loaders.dart
+++ lib/utils/popups/loaders.dart
@@ -5,32 +5,6 @@ import 'package:get/get_core/src/get_main.dart';
 //TODO: Combine into one snack bar?
 
 class AppLoaders{
-  // static successSnackBar({required title, message = '', duration = 3}){
-  //   Get.snackbar(
-  //     title,
-  //     message,
-  //     duration: Duration(seconds: duration),
-  //     snackPosition: SnackPosition.BOTTOM
-  //   );
-  // }
-  //
-  // static warningSnackBar({required title, message = '', duration = 3}){
-  //   Get.snackbar(
-  //     title,
-  //     message,
-  //     duration: Duration(seconds: duration),
-  //     snackPosition: SnackPosition.BOTTOM
-  //   );
-  // }
-  //
-  // static errorSnackBar({required title, message = '', duration = 3}){
-  //   Get.snackbar(
-  //     title,
-  //     message,
-  //     duration: Duration(seconds: duration),
-  //     snackPosition: SnackPosition.BOTTOM
-  //   );
-  // }
 
   static void showSnackBar(BuildContext context, String message){
     ScaffoldMessenger.of(context).showSnackBar(

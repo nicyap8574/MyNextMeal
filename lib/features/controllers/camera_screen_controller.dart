434539b6bfa6b95ff53e32c3d9f975427687ac79
nicyap8574 <nic8574@gmail.com>
2026-04-16 21:46:51 +0800

    Added basic camera functionality

---
+++ lib/features/controllers/camera_screen_controller.dart
@@ -0,0 +1,19 @@
+import 'package:camera/camera.dart';
+
+class CameraScreenController{
+  late CameraController cameraController;
+  late Future<void> initializeControllerFuture;
+
+  Future<void> initCamera(CameraDescription camera) async{
+    cameraController = CameraController(
+      camera,
+      ResolutionPreset.medium,
+    );
+
+    initializeControllerFuture = cameraController.initialize();
+  }
+
+  Future<XFile> uploadCameraImage() async{
+    return await cameraController.takePicture();
+  }
+}
--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,5 +1,11 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
+import 'package:mynextmeal/features/controllers/camera_screen_controller.dart';
+
+import '../utils/constants/colors.dart';
+import '../utils/helpers/helper_functions.dart';
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({super.key, required this.camera});
@@ -11,30 +17,65 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
-    late CameraController _controller;
-    late Future<void> _initializeControllerFuture;
+  late CameraScreenController controller;
 
-    @override
-    void initState(){
-      super.initState();
-      _controller = CameraController(
-        widget.camera,
-        ResolutionPreset.medium,
-      );
+  @override
+  void initState() {
+    super.initState();
 
-      _initializeControllerFuture = _controller.initialize();
-    }
+    controller = Get.put(CameraScreenController());
+    controller.initCamera(widget.camera);
+  }
 
-    @override
-    void dispose(){
-      //dispose controller when widget is disposed
-      _controller.dispose();
-      super.dispose();
-    }
+  //TODO: Dispose camera
+  @override
+  void dispose(){
+    //dispose controller when widget is disposed
+    controller.cameraController.dispose();
+    super.dispose();
+  }
 
-    @override
-    Widget build(BuildContext context) {
-      return Container();
-    }
+  @override
+  Widget build(BuildContext context) {
+    final dark = AppHelperFunctions.isDarkMode(context);
+
+    return Scaffold(
+        backgroundColor: dark ? AppColors.darkBackground : AppColors.lightBackground,
+        appBar: AppBar(
+          title: const Text('Take a picture'),
+        ),
+      body: FutureBuilder(
+        future: controller.initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Stack(
+                children: [
+                  SizedBox.expand(
+                    child: CameraPreview(
+                        controller.cameraController), //shows camera preview
+                  ),
+
+                  //Capture button
+                  Positioned(
+                      bottom: 40,
+                      left: 0,
+                      right: 0,
+                      child: Center(
+                        child: FloatingActionButton(
+                          onPressed: () {
+                          //TODO: Send captured image to image_analysis_controller
+                          },
+                          child: const Icon(Icons.camera),
+                        ),
+                      )
+                  )
+                ]
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        }
+      )
+    );
   }
 }

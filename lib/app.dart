23678b6172291b495bd8ce7b78dff07eaec62505
nicyap8574 <nic8574@gmail.com>
2026-04-17 21:33:19 +0800

    Removed unused imports

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:mynextmeal/utils/constants/colors.dart';
 import 'package:mynextmeal/utils/theme/theme.dart';
-import 'package:mynextmeal/screens/login.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,11 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:mynextmeal/utils/constants/colors.dart';
-import 'package:mynextmeal/utils/theme/theme.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_ai/firebase_ai.dart';
+
 import 'firebase_options.dart';
 
 import 'app.dart';
 import 'data/repositories/authentication_repository.dart';
-import 'firebase_options.dart';
 
 Future<void> main() async {
 

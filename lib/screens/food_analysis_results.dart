deb23575a64bb217b755929672f7c32ec71079e7
nicyap8574 <nic8574@gmail.com>
2026-04-25 23:49:45 +0800

    Centered CircularProgressIndicator

--- lib/screens/food_analysis_results.dart
+++ lib/screens/food_analysis_results.dart
@@ -27,149 +27,153 @@ class FoodAnalysisResults extends StatelessWidget {
         title: const Text('Meal Analysis Results'),
       ),
 
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: AppSpacingStyle.paddingWithAppBarHeight,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Obx((){
+        if(controller.isLoading.value == true){
+          return Center(
+            child: const CircularProgressIndicator(),
+          );
+        }
 
-            children: [
-              Obx((){
-                final imageFile = controller.foodImage.value;
+        return SingleChildScrollView(
+            child: Padding(
+                padding: AppSpacingStyle.paddingWithAppBarHeight,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
-                if(controller.isLoading.value != true && imageFile != null){
-                  return Column(
-                    children: [
-                      Image.file(
-                          File(imageFile.path), //converts XFile to File -> directory to image in device
-                          height: 300,
-                          fit: BoxFit.cover
-                      ),
-                    ],
-                  );
-                }else{
-                  return const SizedBox.shrink();
-                }
-              }),
+                  children: [
+                    Obx((){
+                      final imageFile = controller.foodImage.value;
 
-              const SizedBox(height: AppSizes.spaceBtwSections),
+                      if(controller.isLoading.value != true && imageFile != null){
+                        return Column(
+                          children: [
+                            Image.file(
+                                File(imageFile.path), //converts XFile to File -> directory to image in device
+                                height: 300,
+                                fit: BoxFit.cover
+                            ),
+                          ],
+                        );
+                      }else{
+                        return const SizedBox.shrink();
+                      }
+                    }),
 
-              //Obx so that it updates when response changes and can get the data from repository
-              Obx((){
-                if(controller.isLoading.value == true){
-                  return Center(
-                    child: const CircularProgressIndicator(),
-                  );
-                }else{
-                  if (controller.response.value.isEmpty) {
-                    return const SizedBox.shrink();
-                  }
+                    const SizedBox(height: AppSizes.spaceBtwSections),
 
-                  final data = jsonDecode(controller.response.value);
-                  final nutrients = data['nutrients'] as List<dynamic>;
-                  final meal = nutrients[0] as Map<String, dynamic>;
-                  final mealName = meal['meal_name'];
-                  final ingredients = meal['detected_ingredients'] as List<dynamic>;
-                  final carbsMacro = meal['carbs_macro'];
-                  final proteinMacro = meal['protein_macro'];
-                  final fatsMacro = meal['fats_macro'];
-                  final mealHealthiness = meal['meal_healthiness'];
-                  final confidenceLevel = meal['confidence_level'];
-                  final briefSummary = meal['brief_summary'];
+                    //Obx so that it updates when response changes and can get the data from repository
+                    Obx((){
 
-                  return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Dish Name"),
-                        Chip(
-                          label: Text(mealName),
-                        ),
+                        if (controller.response.value.isEmpty) {
+                          return const SizedBox.shrink();
+                        }
 
-                        const SizedBox(height: AppSizes.spaceBtwItems),
+                        final data = jsonDecode(controller.response.value);
+                        final nutrients = data['nutrients'] as List<dynamic>;
+                        final meal = nutrients[0] as Map<String, dynamic>;
+                        final mealName = meal['meal_name'];
+                        final ingredients = meal['detected_ingredients'] as List<dynamic>;
+                        final carbsMacro = meal['carbs_macro'];
+                        final proteinMacro = meal['protein_macro'];
+                        final fatsMacro = meal['fats_macro'];
+                        final mealHealthiness = meal['meal_healthiness'];
+                        final confidenceLevel = meal['confidence_level'];
+                        final briefSummary = meal['brief_summary'];
 
-                        Text("Ingredients"),
-                        Wrap(
-                          spacing: 8,
-                          children: ingredients.map((individual_ingredient){
-                            return Chip(
-                              label: Text(individual_ingredient),
-                            );
-                          }).toList(), //converts Iterable to List<Widget> to be accepted by children
-                        ),
+                        return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Dish Name"),
+                              Chip(
+                                label: Text(mealName),
+                              ),
 
-                        const SizedBox(height: AppSizes.spaceBtwItems),
+                              const SizedBox(height: AppSizes.spaceBtwItems),
 
-                        Text("Carbs Macro"),
-                        Chip(
-                          label: Text(carbsMacro),
-                        ),
+                              Text("Ingredients"),
+                              Wrap(
+                                spacing: 8,
+                                children: ingredients.map((individual_ingredient){
+                                  return Chip(
+                                    label: Text(individual_ingredient),
+                                  );
+                                }).toList(), //converts Iterable to List<Widget> to be accepted by children
+                              ),
 
-                        const SizedBox(height: AppSizes.spaceBtwItems),
+                              const SizedBox(height: AppSizes.spaceBtwItems),
 
-                        Text("Protein Macro"),
-                        Chip(
-                          label: Text(proteinMacro),
-                        ),
+                              Text("Carbs Macro"),
+                              Chip(
+                                label: Text(carbsMacro),
+                              ),
 
-                        const SizedBox(height: AppSizes.spaceBtwItems),
+                              const SizedBox(height: AppSizes.spaceBtwItems),
 
-                        Text("Fats Macro"),
-                        Chip(
-                          label: Text(fatsMacro),
-                        ),
+                              Text("Protein Macro"),
+                              Chip(
+                                label: Text(proteinMacro),
+                              ),
 
-                        const SizedBox(height: AppSizes.spaceBtwItems),
+                              const SizedBox(height: AppSizes.spaceBtwItems),
 
-                        Text("Meal Healthiness"),
-                        Chip(
-                          label: Text(mealHealthiness),
-                        ),
+                              Text("Fats Macro"),
+                              Chip(
+                                label: Text(fatsMacro),
+                              ),
 
-                        const SizedBox(height: AppSizes.spaceBtwItems),
+                              const SizedBox(height: AppSizes.spaceBtwItems),
 
-                        Text("Confidence Level"),
-                        Chip(
-                          label: Text(confidenceLevel),
-                        ),
+                              Text("Meal Healthiness"),
+                              Chip(
+                                label: Text(mealHealthiness),
+                              ),
 
-                        const SizedBox(height: AppSizes.spaceBtwItems),
+                              const SizedBox(height: AppSizes.spaceBtwItems),
 
-                        Text("Brief Summary"),
-                        Text(briefSummary),
+                              Text("Confidence Level"),
+                              Chip(
+                                label: Text(confidenceLevel),
+                              ),
 
-                        const SizedBox(height: AppSizes.spaceBtwItems),
+                              const SizedBox(height: AppSizes.spaceBtwItems),
 
-                        //save meal
-                        ElevatedButton(
-                            onPressed: () async {
-                              // final data = controller.response.value as Map<String,dynamic>;
-                              final data = jsonDecode(controller.response.value);
-                              final imageUrl = controller.foodImage.value!.path;
-                              await controller.saveMealRecord(data, imageUrl, context);
-                            },
-                            child: const Text("Save Meal"),
-                        ),
+                              Text("Brief Summary"),
+                              Text(briefSummary),
 
-                        //cancel meal save
-                        ElevatedButton(
-                            onPressed: () async {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => const ImageAnalysis()),
-                              );
-                            },
-                            child: const Text("Cancel"),
-                        ),
-                      ]
-                  );
+                              const SizedBox(height: AppSizes.spaceBtwItems),
 
-                  //return Text(repo.response.value);
-                }
-              }),
-            ],
-          )
-        )
-      )
+                              //save meal
+                              ElevatedButton(
+                                onPressed: () async {
+                                  // final data = controller.response.value as Map<String,dynamic>;
+                                  final data = jsonDecode(controller.response.value);
+                                  final imageUrl = controller.foodImage.value!.path;
+                                  await controller.saveMealRecord(data, imageUrl, context);
+                                },
+                                child: const Text("Save Meal"),
+                              ),
+
+                              //cancel meal save
+                              ElevatedButton(
+                                onPressed: () async {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => const ImageAnalysis()),
+                                  );
+                                },
+                                child: const Text("Cancel"),
+                              ),
+                            ]
+                        );
+
+                        //return Text(repo.response.value);
+                      }
+                    ),
+                  ],
+                )
+            )
+        );
+      })
     );
   }
 }

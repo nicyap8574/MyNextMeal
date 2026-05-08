9b751e5160874df3245fbc0a6da79667ed71ce8f
nicyap8574 <nic8574@gmail.com>
2026-05-08 21:17:28 +0800

    Sort meal history in descending order

--- lib/features/controllers/meal_history_controller.dart
+++ lib/features/controllers/meal_history_controller.dart
@@ -2,24 +2,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class MealHistoryController {
-  final FirebaseFirestore _db = FirebaseFirestore.instance;
-  final _auth = FirebaseAuth.instance;
-  List<Map<String, dynamic>> mealList = [];
-
 
   Future<QuerySnapshot<Map<String, dynamic>>> displayCurrentUserMeals() async {
-    final user = _auth.currentUser;
+    final user = FirebaseAuth.instance.currentUser;
 
-    var mealHistoryQuery = await _db.collection('meals').where('user', isEqualTo: user!.uid).get();
-
-    final meal = await FirebaseFirestore.instance
+    var mealHistoryQuery = await FirebaseFirestore.instance
         .collection('meals')
-        .where('user', isEqualTo: user.uid)
+        .where('user', isEqualTo: user!.uid)
+        .orderBy('createdAt', descending: true)
         .get();
-
-    for(var x in meal.docs){
-      mealList.add(x.data());
-    }
 
     return mealHistoryQuery;
     }

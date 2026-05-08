31ce4313c1ccf974bfcb3056fabe76853b84571f
nicyap8574 <nic8574@gmail.com>
2026-05-08 20:25:48 +0800

    Removed unnecessary code

--- lib/screens/meal_history.dart
+++ lib/screens/meal_history.dart
@@ -31,85 +31,49 @@ class MealHistory extends StatelessWidget {
               return Center(child: Text(snapshot.error.toString()));
             }
 
-            //query snapshot (from displayCurrentUserMeals())
+            //query snapshot - documents list (from displayCurrentUserMeals())
             final meals = snapshot.data!.docs;
 
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      FutureBuilder(
-                          future: controller.displayCurrentUserMeals(),
-                          builder: (context, todayMeal){
-                            //todayMeal -> name of snapshot returned by FutureBuilder
+            return ListView.builder(
+                itemCount: meals.length,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context,index){
+                  final meal = meals[index].data(); //JSON output from Firestore
 
-                            if(todayMeal.connectionState == ConnectionState.waiting){
-                              return const Center(child: CircularProgressIndicator());
-                            }
+                  //format date for output
+                  final timestamp = meal['createdAt'];
+                  final date = timestamp.toDate();
+                  final formattedDateTime = DateFormat('dd MMM yyyy, hh:mm a').format(date);
 
-                            if(!todayMeal.hasData || todayMeal.data!.docs.isEmpty){
-                              return const Center(child: Text("No meals found"));
-                            }
+                  return Container(
+                      width: double.infinity,
+                      margin: const EdgeInsets.symmetric(vertical: AppSizes.spaceBtwItems/2, horizontal: 16),
 
-                            if(todayMeal.hasError){
-                              return Center(child: Text(todayMeal.error.toString()));
-                            }
-
-                            final meals = todayMeal.data!.docs;
-
-                            //todayMeal.data -> QuerySnapshot -> All documents inside collection
-                            //.docs -> List<QueryDocumentSnapshot> -> Each document
-
-                            return ListView.builder(
-                              itemCount: meals.length,
-                              shrinkWrap: true,
-                              physics: NeverScrollableScrollPhysics(),
-                              itemBuilder: (context,index){
-                                final meal = meals[index].data(); //JSON output from Firestore
-
-                                //format date for output
-                                final timestamp = meal['createdAt'];
-                                final date = timestamp.toDate();
-                                // final formattedDay = DateFormat('dd MMM yyyy').format(date);
-                                final formattedDate = DateFormat('dd MMM yyyy, hh:mm a').format(date);
-
-                                return Container(
-                                  width: double.infinity,
-                                  margin: const EdgeInsets.symmetric(vertical: AppSizes.spaceBtwItems/2, horizontal: 16),
-
-                                  decoration: BoxDecoration(
-                                    // color: AppColors.lightContainer,
-                                    color: AppColors.white,
-                                    border: Border.all(color: Colors.transparent, width: 0),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: AppColors.darkerGrey.withOpacity(0.1),
-                                        blurRadius: 10,
-                                        offset: Offset(0,4),
-                                      ),
-                                    ],
-                                    borderRadius: BorderRadius.circular(5),
-                                  ),
-
-                                  child: ListTile(
-                                    title: Text(
-                                        meal['analysis']['nutrients'][0]['meal_name'] ?? 'No name',
-                                        style: TextStyle(fontWeight: FontWeight.bold)
-                                    ),
-                                    subtitle: Text(
-                                        "Carbs: ${meal['analysis']['nutrients'][0]['carbs_macro']} | Protein: ${meal['analysis']['nutrients'][0]['protein_macro']} | Fats: ${meal['analysis']['nutrients'][0]['fats_macro']} \n"
-                                        "Uploaded At: $formattedDate"),
-                                  )
-                                );
-                              }
-                            );
-                          }
+                      decoration: BoxDecoration(
+                        color: AppColors.white,
+                        border: Border.all(color: Colors.transparent, width: 0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: AppColors.darkerGrey.withOpacity(0.1),
+                            blurRadius: 10,
+                            offset: Offset(0,4),
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(5),
                       ),
-                    ],
-                  ),
-              ],
+
+                      child: ListTile(
+                        title: Text(
+                            meal['analysis']['nutrients'][0]['meal_name'] ?? 'No name',
+                            style: TextStyle(fontWeight: FontWeight.bold)
+                        ),
+                        subtitle: Text(
+                            "Carbs: ${meal['analysis']['nutrients'][0]['carbs_macro']} | Protein: ${meal['analysis']['nutrients'][0]['protein_macro']} | Fats: ${meal['analysis']['nutrients'][0]['fats_macro']} \n"
+                                "Uploaded At: $formattedDateTime"),
+                      )
+                  );
+                }
             );
           }
       )

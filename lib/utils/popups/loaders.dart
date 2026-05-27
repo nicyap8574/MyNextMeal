d597f36c6cac4cc66b6628b12842abd7e9803106
nicyap8574 <nic8574@gmail.com>
2026-05-27 20:05:28 +0800

    Latest working version

--- lib/utils/popups/loaders.dart
+++ lib/utils/popups/loaders.dart
@@ -12,24 +12,4 @@ class AppLoaders{
         )
     );
   }
-
-  static void showAlertDialog(BuildContext context, String title, String message){
-    showDialog(
-      context: context,
-      builder: (BuildContext context){
-        return AlertDialog(
-          title: Text(title),
-          content: Text(message),
-          actions: [
-            TextButton(
-              onPressed: (){
-                Navigator.of(context).pop();
-              },
-              child: const Text("OK"),
-            )
-          ],
-        );
-      }
-    );
-  }
 }
